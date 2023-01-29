349acc9767fc52aa5c98d43056532fd2a9f491ac
Jimmy <jimmyrss1102@gmail.com>
2023-01-30 01:51:07 +0800

    ci [host] : 修改代理路径

--- vite.config.ts
+++ vite.config.ts
@@ -9,27 +9,20 @@ export default defineConfig(({ command, mode }) => {
   //加载环境变量，打包路由路径
   const env = loadEnv(mode, process.cwd(), '')
   return {
-    // 打包路径生产、开发/gitpages
+    // 打包路径
     base: env.VITE_BASEURL,
     plugins: [react()],
     // 配置路径
     resolve: {
       alias: [{ find: '@', replacement: resolve(__dirname, 'src') }]
     },
-    // vite.config.ts设置代理
-    //https代理https://www.it610.com/article/1404045715889508352.html
     //代理路径
     server: {
-      https: false,
+      hostname: '0.0.0.0',
+      port: 3003,
+      cors: true,
       proxy: {
-        // '/api': 'http://localhost:3003'
-        '/cweb/': {
-          // 开发、发布、gitpages
-          target: 'https://localhost:3003/cweb',
-          secure: false,//不检测https的合法性
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/cweb/, "")
-        },
+        '/admin': 'http://localhost:8080'
       },
     }
   }
