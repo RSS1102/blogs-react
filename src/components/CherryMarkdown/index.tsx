7068de6ee9960c2209f321ae41f122ab6eb174d1
Jimmy <jimmyrss1102@gmail.com>
2023-04-01 03:41:38 +0800

    feat : 获取 `cherry` 的数据

--- src/components/CherryMarkdown/index.tsx
+++ src/components/CherryMarkdown/index.tsx
@@ -1,11 +1,6 @@
-import "cherry-markdown/dist/cherry-markdown.css"
 import Cherry from "cherry-markdown";
 import { toolbarsOptions } from "./cherryMarkdownConfig";
-const cherryMarkdownInit =()=>{
-   new Cherry({
-        id: 'cherry-markdown',
-        value: 'welcome',
-        toolbars: toolbarsOptions(),
-    });
+const cherryMarkdownInit = () => {
+
 }
 export default cherryMarkdownInit;
--- src/pages/publishArticle/index.tsx
+++ src/pages/publishArticle/index.tsx
@@ -1,52 +1,58 @@
 import React, { useEffect, useState } from 'react';
 import "./index.scss"
 import { Card, Form, Input, Button, Select, message, Modal, Divider } from 'antd';
-import cherryMarkdownInit from '@/components/CherryMarkdown';
 import { blogNavType } from '@/types/classifyType';
 import { ArticleFormType } from '@/types/articleType'
+import Cherry from 'cherry-markdown';
+import { toolbarsOptions } from '@/components/CherryMarkdown/cherryMarkdownConfig';
 const { Option } = Select
 const PublishArticle: React.FC = () => {
     const [blogNavVal, setBlogNavVal] = useState<Array<blogNavType>>()
     const [isModalVisible, setIsModalVisible] = useState<boolean>(false)
-
+    const [cherry, setCherry] = useState<Cherry>();
     useEffect(() => {
-        cherryMarkdownInit();
+        setCherry(new Cherry({
+            id: 'cherry-markdown',
+            value: 'init',
+            toolbars: toolbarsOptions(),
+        }));
     }, [])
-    // 获取文章
+
     const [form] = Form.useForm<ArticleFormType>();
-    let onEdit = (editor: string) => {
-        form.setFieldsValue({ blogContent: editor })
-    }
+
     // 发布文章
-    const onPublic = () => {
-        form.validateFields().then(async (values: ArticleFormType) => {
-            setIsModalVisible(true)
-        }).catch(err => { })
+    const validArticle = () => {
+        console.log(cherry!.getHtml())
+        form.validateFields()
+            .then(async (values: ArticleFormType) => {
+                setIsModalVisible(true)
+            })
     }
-    const onFinishCancel = () => {
+    const onPublic = () => {
         setIsModalVisible(false)
     }
-    const onFinish = () => {
+    const unFinish = () => {
+
     }
+
     return (
         <>
-            <Modal title="是否发布文章" open={isModalVisible} onOk={onFinish} onCancel={onFinishCancel}>
+            <Modal title="是否发布文章" open={isModalVisible} onOk={onPublic} onCancel={unFinish}>
                 <p>请仔细核对发布的内容。</p>
             </Modal>
-            <Form initialValues={{ blogContent: null }} form={form} >
+            <Form form={form} >
                 <Card >
-                    <Form.Item label="标题" name='blogTitle'
+                    <Form.Item label="标题" name='title'
                         rules={[
                             { required: true, message: '请输入标题' },
                             { max: 20, message: '标题最多20个字符' }
                         ]}>
                         <div style={{ display: 'flex' }}>
                             <Input style={{ width: '100%' }} placeholder="请输入标题" />
-                            <Button style={{ marginLeft: '25px' }} size='large' type='primary'
-                                onClick={onPublic}>发布</Button>
+                            <Button style={{ marginLeft: '25px' }} size='large' type='primary' onClick={validArticle}>发布</Button>
                         </div>
                     </Form.Item>
-                    <Form.Item label="分类" name='blogNavs'
+                    <Form.Item label="分类" name='groupId'
                         rules={[{ required: true, message: '请选择分类' }]}>
                         <Select style={{ width: '100%' }} allowClear labelInValue>
                             {blogNavVal?.map(item =>
@@ -54,9 +60,8 @@ const PublishArticle: React.FC = () => {
                         </Select>
                     </Form.Item>
                 </Card>
-                <Divider >文章</Divider>
                 <Card >
-                    <Form.Item  name='blogContent'
+                    <Form.Item name='content'
                         rules={[
                             { min: 50, message: '文章最最少50个字符' },
                             { max: 20000, message: '文章最多20000个字符' },
